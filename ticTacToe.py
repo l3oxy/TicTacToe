008b84dffe01f47fd60be2fed1615669d6c21e60
l3oxy <42228543+l3oxy@users.noreply.github.com>
2018-08-11 21:17:41 -0500

    Resolved bugs 1,2,3,4,6. Made the game 2 player. Trying out how to use a reset.

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,9 +1,9 @@
-# The purpose of this program is to play a game of tic tac toe.
+# Tic-Tac-Toe
+# by l3oxy
+# The purpose of this program is to play a game of Tic-Tac-Toe.
 
-# Define quit condition that a user can use to end the program right away
-def does_player_want_quit(userInput):
-    if str(userInput).upper() == "QUIT":
-        exit()
+# Module importations
+import os # This is used by clear_screen()
 
 # The purpose of this function is to print an introduction message.
 def display_introduction():
@@ -11,15 +11,59 @@ def display_introduction():
     print(introduction_message)
     print("\nEnter 'QUIT' at any time to quit.")
 
+# The purpose of this function is to clear the CLI
+def clear_screen():
+    # Save the output to a recycleBin variable, else the exit-code is printed.
+    recycleBin = os.system("clear" if os.name=="posix" else "cls")
+
+# Construct the game board, and return it.
+def construct_board(ICONS_OPEN):
+    # Define variables
+    board = []
+    board_rows = 3
+    board_columns = 3
+
+    # Construct the board using the variables above
+    for current_row in range(board_rows):
+        board.append([])
+        for current_column in range(board_columns):
+            board[current_row].append(ICONS_OPEN)
+
+    # Return the board back to the function that calls this function
+    return(board)
+
+# Display the current board
+def display_board(board):
+    for row in board:
+        for coord in row:
+            print(" " + coord, end="")
+        print("\n")
+
+# The purpose of this function is to update the screen.
+def update_screen(board, also_print_this=""):
+    clear_screen()
+    display_introduction()
+    display_board(board)
+    if also_print_this:
+        print(also_print_this)
+
+# Define quit condition that a user can use to end the program right away
+def does_player_want_quit(userInput):
+    if userInput.upper() == "QUIT":
+        exit()
+
 # Have all players choose their icons (e.g. X, O, 7, etc), then return the list.
-def pick_icons(number_of_players):
+# Returns a list,
+#  The 1st value within the list is a list of player icons.
+#  The 2nd value within the lsit is a string of the 'open' icon.
+def pick_player_icons(number_of_players):
     # Define variables.
     player_icons = []
 
     for playerNumber in range(1, (number_of_players + 1)):
         ask_player_for_input = "Player" + str(playerNumber) + ", pick your icon: "
-        current_player_icon_choise_is_valid = False
 
+        current_player_icon_choise_is_valid = False
         while current_player_icon_choise_is_valid != True:
             # Capture player icon choise input.
             playerInput = input(ask_player_for_input)
@@ -30,7 +74,7 @@ def pick_icons(number_of_players):
             # Verify current player's icon choise has a length of 1.
             playerInputLength = len(playerInput)
             if playerInputLength != 1:
-                # If icon choise is too large/long.
+                # If icon choise is too large/long tell the player.
                 if playerInputLength > 1:
                     print("INVALID: Requires length of 1")
                 continue
@@ -44,96 +88,81 @@ def pick_icons(number_of_players):
             # this while loop can be ended.
             current_player_icon_choise_is_valid = True
 
+        # Add the new player icon to the list of player icons.
         player_icons.append(playerInput)
 
+    # Return the list of player icons.
     return(player_icons)
 
-# Construct the game board, and return it.
-def construct_board(icons_open):
-    # Define variables
-    board = []
-    board_rows = 3
-    board_columns = 3
+# The purpose of this function is to verify that an input is a valid coordinate
+# Returns a dict that says what the value is , and if it is valid.
+def is_valid_coordinate(test_value):
+    # Define variable
+    accaptable_ints = [0,1,2]
+    validity_test = {"int":None, "is_valid?":False}
 
-    # Construct the board using the variables above
-    for current_row in range(board_rows):
-        board.append([])
-        for current_column in range(board_columns):
-            board[current_row].append(icons_open)
+    # Verify that it is not the quit code
+    does_player_want_quit(test_value)
 
-    # Return the board back to the function that calls this function
-    return(board)
+    # Test if the value is valid
+    if test_value.isdigit():
+        validity_test["int"] = int(test_value)
+        if validity_test["int"] in accaptable_ints:
+            validity_test["is_valid?"] = True
 
-# Display the current board
-def display_board(in_board):
-    for row in in_board:
-        for spot in row:
-            print(spot, end=" ")
-        print("\n")
-
-# The purpose of this function is to verify 
-def verify_coordinate_input(in_value):
-    does_player_want_quit(in_value)
-    test_value = int(in_value)
-    if test_value in [0, 1, 2]:
-        good_value = test_value
-        #print("VALID: " + str(in_value) + " -> " + str(good_value)) #DEBUG
-        return(good_value)
-    else:
-        #print("NOT VALID: " + str(in_value)) #DEBUG
-        return("Fail")
-
+    # Return the results
+    return(validity_test)
 
 # The purpose of this function is to prompt for a coordinate, verify if it ...
 # meets the requirements, and if so, return it as an int.
-def get_coordinate(axis):
+def get_coordinate(ICONS_PLAYERS, current_player, axis):
     # Prepare prompt
-    message = "Enter " + axis + " (0-2): "
+    message = "[" + ICONS_PLAYERS[current_player] + "] Choose " + axis + " (0-2): "
 
     # Enter loop, asking for input, testing it, and repeating until tests successfully.
-    verification_result = "Fail"
-    while verification_result == "Fail":
-        verification_result = verify_coordinate_input(input(message))
-    verified_input = verification_result
+    coordinate_test = is_valid_coordinate(input(message))
+    while coordinate_test["is_valid?"] != True:
+        coordinate_test = is_valid_coordinate(input(message))
+
+    verified_input = coordinate_test["int"]
     return(verified_input)
 
 
-# The purpose of this function is to place an X/O (depending on who the player is).
-def pick(in_board, icons_open, row, col):
+# The purpose of this function is to place the current player's icon.
+def pick(board, current_player, ICONS, row, col):
     response = "[" + str(row) + "][" + str(col) + "] "
 
-    if in_board[row][col] == icons_open:
+    if board[row][col] == ICONS["OPEN"]:
         # Spot is open.
-        in_board[row][col] = "X" #FIXME: This should be the real players icon
-        display_board(in_board)
-        return(in_board)
+        board[row][col] = ICONS["PLAYERS"][current_player]
+        update_screen(board)
+        return(board)
     else:
         # Spot is taken.
-        print(response + "is already occupied by " + str(in_board[row][col]))
-        display_board(in_board)
+        print(response + "is already occupied by " + str(board[row][col]))
         return(False)
 
-# The purpose of this function is to prompt the player (whose turn it is) to
+# The purpose of this function is to prompt the current player (whomever that is) to
 # choose the location for their next icon placement. Then place it there.
-def play_turn(board, icons_open):
+def play_turn(board, current_player, ICONS):
     # Define variables
     board_update = False
 
-    # Get the coordinates for the next icon placement, and call the function to place them.
+    # Get the coordinates for the next icon placement, and call the function to place it.
     while board_update is False:
-        row = get_coordinate("row")
-        col = get_coordinate("column")
-        board_update = pick(board, icons_open, row, col)
+        row = get_coordinate(ICONS["PLAYERS"], current_player, "row")
+        col = get_coordinate(ICONS["PLAYERS"], current_player, "column")
+        board_update = pick(board, current_player, ICONS, row, col)
 
     # Return the now update board
     return(board_update)
 
 
-# Detect if a player has won horizontaly
+# Detect if a player has won horizontaly, and if so then whom
 # If a player has won horizontaly, return the victor's icon
-# If no player has won horizontaly, return False
-def victory_horizontal(board, icons_players):
-    winner = False
+# If no player has won horizontaly, return None
+def victory_horizontal(board, ICONS_PLAYERS):
+    detected_horizontal_winner = None
 
     for row in board:
         icon_left = row[0]
@@ -141,140 +170,187 @@ def victory_horizontal(board, icons_players):
         icon_right = row[2]
 
         # Test that the left icon is a player (in contrast to being open/unused)
-        if icon_left in icons_players:
+        if icon_left in ICONS_PLAYERS:
             # Test if the left, middle, and right, are all the same player
             if icon_left == icon_mid and icon_left == icon_right:
                 # If so, then return the winning player
-                winner = icon_left
-                return(winner)
+                detected_horizontal_winner = icon_left
 
-    # If the control flow gets to this point, then there is no horizontal winner
-    return(False)
+    return(detected_horizontal_winner)
 
 # The purpose of this function is to detect if a player has win verticaly.
 # If a player has won verticaly, return the victor's icon.
-# If no player has won verticaly, return False.
-def victory_verticle(board, icons_players):
-    total_number_of_columns = 3
-    winner = False
+# If no player has won verticaly, return None.
+def victory_vertical(board, ICONS_PLAYERS):
+    # Define variables
+    total_number_of_columns = len(board[0])
+    detected_vertical_winner = None
 
+    # For each column
     for column in range(total_number_of_columns):
+        # Capture all rows within the current column
         icon_top = board[0][column]
         icon_middle = board[1][column]
         icon_bottom = board[2][column]
 
-        if icon_top in icons_players:
+        # Check if all in column are the same player
+        if icon_top in ICONS_PLAYERS:
             if icon_middle == icon_top:
                 if icon_bottom == icon_top:
-                    winner = icon_top
-                    return(winner)
+                    detected_vertical_winner = icon_top
+                    break
 
-    # This only happens if there is no winner
-    return(False)
+    return(detected_vertical_winner)
+
+# Check if top-left to bottom-right victory
+# The winner, else if no winner this returns None
+def victory_diagonaly_topLeft_to_bottomRight(board, ICONS_PLAYERS):
+    # Define variables
+    icon_top_left = board[0][0]
+    icon_center = board[1][1]
+    icon_bottom_right = board[2][2]
+    detected_TLBR_winner = None  # TLBR is shorthand for 'Top-Left to Bottom-Right'
+
+    # Test if the top-left is a player icon
+    if icon_top_left in ICONS_PLAYERS:
+        # If it is a player icon, then test if others are the same
+        if icon_center == icon_top_left and icon_bottom_right == icon_top_left:
+            detected_TLBR_winner = icon_top_left
+
+    return(detected_TLBR_winner)
+
+# Check if top-right to bottom-left victory
+# If there is a winner, return winner's icon. Else return None
+def victory_diagonaly_topRight_to_bottomLeft(board, ICONS_PLAYERS):
+    # Define variable
+    icon_top_right = board[0][-1]
+    icon_center = board[1][1]
+    icon_bottom_left = board[-1][0]
+    detected_TRBL_winner = None # TRBL is shorthand for 'Top-Right to Bottom-Left'
+
+    # Test if the top-right is a player icon
+    if icon_top_right in ICONS_PLAYERS:
+        # If it is a player icon, then test if others are the same
+        if icon_center == icon_top_right and icon_bottom_left == icon_top_right:
+            detected_TRBL_winner = icon_top_right
+
+    return(detected_TRBL_winner)
 
 # Detect if a player has won diagonaly
-# If a player has won diagonaly, then return the victor's icon
-# If no player has won diagonaly, then return False
-def victory_diagonaly():
-    #FIXME
-    return(False)
+# If a player diagonaly this returns the winner's icon, ...
+# ... else if there is no winner, this returns None
+def victory_diagonal(board, ICONS_PLAYERS):
+    # Define variables
+    TopLeft_to_BottomRight_test = victory_diagonaly_topLeft_to_bottomRight(board, ICONS_PLAYERS)
+    TopRight_to_BottomLeft_test = victory_diagonaly_topRight_to_bottomLeft(board, ICONS_PLAYERS)
+    diagonal_test_result = None
+
+    # Review test results, and return any winner
+    if TopLeft_to_BottomRight_test in ICONS_PLAYERS:
+        diagonal_test_result = TopLeft_to_BottomRight_test
+    elif TopRight_to_BottomLeft_test in ICONS_PLAYERS:
+        diagonal_test_result = TopRight_to_BottomLeft_test
+
+    return(diagonal_test_result)
 
 
 # The purpose of this function is to test if either player has won.
 # If there is a winner, return the winner's icon.
 # If there is not a winner, return False.
-def has_anyone_won(board, icons_players, icons_open):
-    winner_horizontal = victory_horizontal(board, icons_players)
+def has_anyone_won(board, ICONS_PLAYERS, ICONS_OPEN):
+    # Define variables
+    victory_result = {"win_detected?":False, "winner":None}
 
-    if winner_horizontal in icons_players:
-        return(winner_horizontal)
-    else:
-        winner_verticle = victory_verticle(board, icons_players)
+    # For each way to win (verticle, horizontal, diagonal), test that way.
+    for victory_method in [victory_horizontal, victory_vertical, victory_diagonal]:
+        victory_method_result = victory_method(board, ICONS_PLAYERS)
+        if victory_method_result in ICONS_PLAYERS:
+            victory_result["winner"] = victory_method_result
+            victory_result["win_detected?"] = True
+            break
 
-        if winner_verticle in icons_players:
-            return(winner_verticle)
-        else:
-            return(False)
-
+    # Return dict that lists if any wins were detected, and if so includes the winner.
+    return(victory_result)
 
 # The purpose of this function is to test if there are any spots available.
-def any_spots_available(board, icons_open):
+def board_out_of_spots(board, ICONS_OPEN):
     for row in board:
-        for spot in row:
-            if spot == icons_open:
-                return(True)
+        for coordinate in row:
+            if coordinate == ICONS_OPEN:
+                return(False)
 
-    print("NO MORE SPOTS")
-    return(False)
+    return(True)
 
 # The purpose of this function is to test if the game is complete or not.
 # If complete and a player won, return victor's icon
 # If complete and a tie, return True
-# If not complete, return False
-def is_game_complete(board, icons_players, icons_open):
-    winner = has_anyone_won(board, icons_players, icons_open)
+# If not complete yet, return False
+def is_game_complete(board, ICONS_PLAYERS, ICONS_OPEN):
+    has_anyone_won_result = has_anyone_won(board, ICONS_PLAYERS, ICONS_OPEN)
 
-    if winner in icons_players:
-        game_winner = winner
+    if has_anyone_won_result["win_detected?"]:
+        game_winner = has_anyone_won_result["winner"]
         return(game_winner)
     else:
-        if any_spots_available(board, icons_open):
-            return(False)
-        else:
-            return(True)
+        return(board_out_of_spots(board, ICONS_OPEN))
+
+# The purpose of this function is to select the next player.
+# Expects current_player to be 0-based
+# Expects NUMBER_OF_PLAYERS to be 1-based
+def next_player(current_player, NUMBER_OF_PLAYERS):
+    # new_current_player should be (current_player + 1),
+    # unless it would be out of the range of NUMBER_OF_PLAYERS, in which case set to 0.
+    if (current_player + 1) >= NUMBER_OF_PLAYERS:
+        new_current_player = 0
+    else:
+        new_current_player = (current_player + 1)
+
+    return(new_current_player)
 
 # Go back and forst between all players, having each take a turn until the complete ends.
-def play_game(board, icons_players, icons_open):
+def play_game(board, NUMBER_OF_PLAYERS, ICONS):
+    # Define variables
     current_player = 0
-    number_of_players = 2 # FIXME: This should be somewhere else I think.
     game_completion_status = False
+    ICONS_OPEN = ICONS["OPEN"]
+    ICONS_PLAYERS = ICONS["PLAYERS"]
 
     while game_completion_status == False:
         # Let current_player play their turn
-        play_turn(board, icons_open)
-
-        # Rotate current player
-        current_player += 1
-        if current_player >= number_of_players:
-            current_player = 0
+        play_turn(board, current_player, ICONS)
 
         # Check if game is complete
-        game_completion_status = is_game_complete(board, icons_players, icons_open)
+        game_completion_status = is_game_complete(board, ICONS_PLAYERS, ICONS_OPEN)
+
+        # Rotate current player
+        current_player = next_player(current_player, NUMBER_OF_PLAYERS)
 
     return(game_completion_status)
 
-def display_result(result):
-    1+1
-    #game_complete = is_game_complete(board, player_icons, icons_open)
-    #while game_complete == False:
-    #    play_turn(current_player)
-    #    game_complete = is_game_complete(board, player_icons, icons_open)
-    #
-    #if game_complete in icons_players:
-    #    print("Congratulations!\nThe winner is " + str(game_complete))
-    #    exit()
-    #elif game_complete:
-    #    print("That's a Tie! Good Game!")
-    #    exit()
-    #else:
-    #    print("Error: Game ended but cannot determine result")
-    #    exit()
-    #print("display_result() line 209 about somehting FIXME") #FIXME
+# The purpose of this function is to display why the game ended.
+def display_result(board, RESULT, ICONS_PLAYERS):
+    if RESULT in ICONS_PLAYERS:
+        update_screen(board, "Congratulations!\nThe winner is " + RESULT)
+    elif RESULT == True:
+        # If there are no spots available, and nobody has won.
+        update_screen(board, "That's a Tie! Good Game!")
+    else:
+       print("Error: Game ended but cannot determine result")
+       exit()
 
 # The purpose of this function is to be the mainline/controller function.
 def mainline():
     # Define variables
-    game_winner = False
-    number_of_players = 2
-    icons_open = "_"
+    NUMBER_OF_PLAYERS = 2
+    ICONS = {"OPEN": "_"}
 
-    #
+    # Call functions
     display_introduction()
-    player_icons = pick_icons(number_of_players)
-    board = construct_board(icons_open)
+    board = construct_board(ICONS["OPEN"])
     display_board(board)
-    result = play_game(board, player_icons, icons_open)
-
-    #display_result(result) # Wait until display_result is functional before calling it here.
+    ICONS["PLAYERS"] = pick_player_icons(NUMBER_OF_PLAYERS)
+    update_screen(board)
+    RESULT = play_game(board, NUMBER_OF_PLAYERS, ICONS)
+    display_result(board, RESULT, ICONS["PLAYERS"])
 
 mainline()
